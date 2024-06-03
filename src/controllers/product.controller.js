46e8f2da88cafcdd9ccb118f1a30f7093991e1cb
tn823 <trnghia823@gmail.com>
2024-06-03 12:00:34 +0700

    feat: create add product api

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -10,3 +10,24 @@ exports.getAllProducts = async (req, res) => {
         res.status(500).json({ error: 'Error while getting products' });
     }
 };
+
+// Thêm sản phẩm mới vào cơ sở dữ liệu
+exports.createProduct = async (req, res) => {
+    try {
+        const { name, description, price, category_id, img } = req.body;
+
+        // Tạo một sản phẩm mới
+        const newProduct = await Product.create({
+            name,
+            description,
+            price,
+            category_id,
+            img
+        });
+
+        res.status(201).json({ message: "Thêm sản phẩm thành công !" }); // Trả về sản phẩm mới đã được tạo
+    } catch (error) {
+        console.error('Error while creating product:', error);
+        res.status(500).json({ error: 'Error while creating product' });
+    }
+}
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -5,5 +5,6 @@ const productController = require('../controllers/product.controller')
 //Route to get all products
 router.get('/products', productController.getAllProducts);
 
+router.post('/appProduct', productController.createProduct);
 
 module.exports = router;
