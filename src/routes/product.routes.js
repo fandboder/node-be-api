fdab0e6162b7c39a0a6f166ad68f82ef9daeff47
Minh <elevenine00@gmail.com>
2024-06-03 15:05:33 +0700

    style: change format

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const productController = require('../controllers/product.controller')
 
-//Route to get all products
 router.get('/getProducts', productController.getAllProducts);
 
 router.post('/addProduct', productController.createProduct);
