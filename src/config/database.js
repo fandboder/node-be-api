d5f1e9e5e38c23dfddb3574c53e2c9df1a07319a
Minh <elevenine00@gmail.com>
2024-06-12 18:01:00 +0700

    hotfix: "config database"

--- src/config/database.js
+++ src/config/database.js
@@ -6,6 +6,8 @@ dotenv.config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
+    dialectModule: require('mysql2'), 
+
 });
 
 const connectDB = async () => {
