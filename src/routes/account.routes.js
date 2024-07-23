3b3ce77ae43ecfbfc02068427c4bc2d2734dbc47
tn823 <trnghia823@gmail.com>
2024-07-23 19:03:23 +0700

    update endpoint

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -4,15 +4,15 @@ const accountController = require('../controllers/account.controller');
 
 router.get('/accounts', accountController.getAllAccounts);
 
-router.post('/account', accountController.addAccount);
+router.post('/accounts', accountController.addAccount);
 
-router.delete('/account/:id', accountController.deleteAccount);
+router.delete('/accounts/:id', accountController.deleteAccount);
 
-router.put('/account/:id', accountController.updateAccount);
+router.put('/accounts/:id', accountController.updateAccount);
 
 router.post('/login', accountController.login);
 
-router.get('/account/:id', accountController.getAccountById);
+router.get('/accounts/:id', accountController.getAccountById);
 
 router.get('/accounts/username/:username', accountController.getAccountsByUsername);
 
