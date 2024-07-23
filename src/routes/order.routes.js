0254387fed996c2db5e0c0c3ee4c5973d2fa7e5e
tn823 <trnghia823@gmail.com>
2024-07-23 20:37:05 +0700

    update endpoint

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/order.controller');
 
-router.post('/order', orderController.createOrder);
+router.post('/orders', orderController.createOrder);
 
 router.get('/orders/:orderId', orderController.getOrderById);
 
